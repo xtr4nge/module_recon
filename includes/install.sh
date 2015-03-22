6d411a08090a8a59d4f9c04eeee811f2f74e1f0b
xtr4nge <xtr4nge@gmail.com>
2015-03-22 12:29:16 +0000

    Initial commit

--- includes/install.sh
+++ includes/install.sh
@@ -2,9 +2,9 @@
 
 echo "installing mitmproxy..."
 
-apt-get -f install python-pip
+apt-get -y install python-pip
 
-apt-get -f install build-essential python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev
+apt-get -y install build-essential python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev
 
 pip install --upgrade six
 

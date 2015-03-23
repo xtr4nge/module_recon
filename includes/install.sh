0fc3e3a30eea23c17b598b84f89736c69acfbba2
xtr4nge <xtr4nge@gmail.com>
2015-03-23 18:49:39 +0000

    Initial commit

--- includes/install.sh
+++ includes/install.sh
@@ -2,16 +2,17 @@
 
 echo "installing mitmproxy..."
 
-apt-get -y install python-pip
+apt-get -y install python-pip php5-sqlite
 
 apt-get -y install build-essential python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev
 
 pip install --upgrade six
 
-
 #https://pypi.python.org/packages/source/u/urwid/urwid-1.3.0.tar.gz # if error, install urwid manually first. (http://urwid.org/)
 
 pip install mitmproxy
+
+chmod 777 ../www.recon/db/
 
 echo "..DONE.."
 exit
